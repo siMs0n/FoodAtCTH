269d4eeef39c8c16b7af20fdb283e7312a59dbb2
siMs0n <simon.c.nielsen@gmail.com>
2015-06-02 20:23:13 +0200

    Added a toolbar and drawer but not compiling

--- FoodAtCTH/app/src/main/java/com/nielsen/simon/foodatcth/MainActivity.java
+++ FoodAtCTH/app/src/main/java/com/nielsen/simon/foodatcth/MainActivity.java
@@ -2,15 +2,24 @@ package com.nielsen.simon.foodatcth;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+
+        NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
+
     }
 
     @Override
