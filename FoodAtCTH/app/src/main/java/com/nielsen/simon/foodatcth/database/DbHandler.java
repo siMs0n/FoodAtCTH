2b4b96da0d82fb52c0e0de7eed0c317618c807c6
siMs0n <simon.c.nielsen@gmail.com>
2015-07-20 18:34:16 +0200

    Added a database and updated database code

--- FoodAtCTH/app/src/main/java/com/nielsen/simon/foodatcth/database/DbHandler.java
+++ FoodAtCTH/app/src/main/java/com/nielsen/simon/foodatcth/database/DbHandler.java
@@ -5,18 +5,26 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import com.nielsen.simon.foodatcth.Message;
+import com.nielsen.simon.foodatcth.R;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.Enumeration;
 import java.util.Map;
 
 /**
  * Created by Simon on 2015-07-20.
  */
 public class DbHandler extends SQLiteOpenHelper {
+
+    //The Android's default system path of your application database.
+    private static String DB_PATH = "/data/data/YOUR_PACKAGE/databases/";
 
     private static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME = "menusDB.db";
@@ -48,9 +56,9 @@ public class DbHandler extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         try {
-            db.execSQL(CREATE_SANNE_TABLE);
-        } catch (SQLException e) {
-            Message.simpleMessage(context, "" + e);
+            createDataBase();
+        } catch (IOException e) {
+            Message.simpleMessage(context, context.getResources().getString(R.string.menu_error));
         }
         tables.put(Menu.SANNE, TABLE_SANNE);
         tables.put(Menu.FAIJTAS, TABLE_FAIJTAS);
@@ -59,12 +67,87 @@ public class DbHandler extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         try {
-            db.execSQL("DROP TABLE IF EXISTS " + TABLE_SANNE);
-            db.execSQL("DROP TABLE IF EXISTS " + TABLE_FAIJTAS);
-            onCreate(db);
+            context.deleteDatabase(DATABASE_NAME);
+            onCreate(null);
         } catch (SQLException e) {
-            Message.simpleMessage(context, "" + e);
+            Message.simpleMessage(context, context.getResources().getString(R.string.menu_error));
         }
+    }
+
+    private void createDataBase() throws IOException {
+
+        boolean dbExist = checkDataBase();
+
+        if (!dbExist) {
+
+            //By calling this method and empty database will be created into the default system path
+            //of your application so we are gonna be able to overwrite that database with our database.
+            this.getReadableDatabase();
+
+            try {
+
+                copyDataBase();
+
+            } catch (IOException e) {
+
+                Message.simpleMessage(context, context.getResources().getString(R.string.menu_error));
+
+            }
+        }
+
+    }
+
+    /**
+     * Check if the database already exist to avoid re-copying the file each time you open the application.
+     *
+     * @return true if it exists, false if it doesn't
+     */
+    private boolean checkDataBase() {
+
+        SQLiteDatabase checkDB = null;
+
+        try {
+            String myPath = DB_PATH + DATABASE_NAME;
+            checkDB = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+        } catch (SQLiteException e) {
+            //database does't exist yet.
+        }
+
+        if (checkDB != null) {
+            checkDB.close();
+        }
+
+        return checkDB != null ? true : false;
+    }
+
+    /**
+     * Copies the database from the local assets-folder to the just created empty database in the
+     * system folder, from where it can be accessed and handled.
+     * This is done by transfering bytestream.
+     */
+    private void copyDataBase() throws IOException {
+
+        //Open your local db as the input stream
+        InputStream myInput = context.getAssets().open(DATABASE_NAME);
+
+        //Path to the just created empty db
+        String outFileName = DB_PATH + DATABASE_NAME;
+
+        //Open the empty db as the output stream
+        OutputStream myOutput = new FileOutputStream(outFileName);
+
+        //Transfer bytes from the inputfile to the outputfile
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = myInput.read(buffer)) > 0) {
+            myOutput.write(buffer, 0, length);
+        }
+
+        //Close the streams
+        myOutput.flush();
+        myOutput.close();
+        myInput.close();
+
     }
 
     public boolean addMenuItem(Menu menu, Product product) {
