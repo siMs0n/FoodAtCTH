aaa396dc75ec71f90d360985df6256552e430d11
siMs0n <simon.c.nielsen@gmail.com>
2015-07-26 21:20:18 +0200

    Fixed list for pizza menu

---
+++ FoodAtCTH/app/src/main/java/com/nielsen/simon/foodatcth/PizzaMenuAdapter.java
@@ -0,0 +1,83 @@
+package com.nielsen.simon.foodatcth;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Simon on 2015-07-26.
+ */
+public class PizzaMenuAdapter extends RecyclerView.Adapter<PizzaMenuAdapter.ViewHolder> {
+
+    private ArrayList<Pizza> menu;
+
+    private static final int LIST_ITEM = 0;
+    private static final int LIST_TITLE = 1;
+    private static final int LIST_HEADER = 2;
+
+    public PizzaMenuAdapter() {
+        menu = new ArrayList<>();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView nr, name, ingredients, price;
+        public int view_type;
+
+        public ViewHolder(View itemView, int viewType) {
+            super(itemView);
+            view_type = viewType;
+            if(viewType == LIST_ITEM) {
+                nr = (TextView)itemView.findViewById(R.id.pizzaNr);
+                name = (TextView)itemView.findViewById(R.id.pizzaName);
+                ingredients = (TextView)itemView.findViewById(R.id.pizzaIngredients);
+                price = (TextView)itemView.findViewById(R.id.pizzaPrice);
+            }
+        }
+    }
+
+    public void setMenu(ArrayList<Pizza> menu){
+        this.menu = menu;
+    }
+
+    public ArrayList<Pizza> getMenu(){
+        return menu;
+    }
+
+
+    @Override
+    public PizzaMenuAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_pizza, parent, false); //Inflating the layout
+
+        return new ViewHolder(v, viewType);
+    }
+
+    //Next we override a method which is called when the item in a row is needed to be displayed, here the int position
+    // Tells us item at which position is being constructed to be displayed and the holder id of the holder object tell us
+    // which view type is being created 1 for item row
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.nr.setText(menu.get(position).getNr()+".");
+        holder.name.setText(menu.get(position).getName());
+        holder.ingredients.setText(menu.get(position).getIngredients());
+        holder.price.setText(menu.get(position).getPrice() + " kr");
+    }
+
+    // This method returns the number of items present in the list
+    @Override
+    public int getItemCount() {
+        return menu.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return LIST_ITEM;
+    }
+
+}
+
--- FoodAtCTH/app/src/main/java/com/nielsen/simon/foodatcth/SannegardenGibraltar.java
+++ FoodAtCTH/app/src/main/java/com/nielsen/simon/foodatcth/SannegardenGibraltar.java
@@ -2,7 +2,10 @@ package com.nielsen.simon.foodatcth;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.TextView;
 
 import com.nielsen.simon.foodatcth.database.DbHandler;
@@ -17,20 +20,32 @@ public class SannegardenGibraltar extends AppCompatActivity {
     private Toolbar toolbar;
     private ArrayList<Pizza> menu;
 
+    RecyclerView menuRecyclerView;
+    PizzaMenuAdapter menuAdapter;
+    RecyclerView.LayoutManager menuLayoutManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sannegarden_gibraltar);
 
-        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        toolbar = (Toolbar) findViewById(R.id.pizza_app_bar);
         setSupportActionBar(toolbar);
+
+        //Set up basic menu ------------------------------------------------------
+
+        menuRecyclerView = (RecyclerView) findViewById(R.id.MenuRecyclerView);
+
+        menuAdapter = new PizzaMenuAdapter();
+        menuRecyclerView.setAdapter(menuAdapter);
+        //menuRecyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+        menuLayoutManager = new LinearLayoutManager(this);
+        menuRecyclerView.setLayoutManager(menuLayoutManager);
+        // End set up basic menu -------------------------------------------------
 
         DbHandler dbHandler = new DbHandler(this);
         menu = dbHandler.getPizzaMenu(DbHandler.PizzaMenu.SANNE_GIBRALTAR);
-        TextView txt = ((TextView)findViewById(R.id.txtView));
-        for(int i = 0; i<menu.size(); i++){
-            txt.setText(txt.getText().toString()+menu.get(i).getNr()+menu.get(i).getName()+menu.get(i).getIngredients()+menu.get(i).getPrice()+" "+menu.get(i).getGroupNr()+"\n");
-        }
+        menuAdapter.setMenu(menu);
     }
 
 }
